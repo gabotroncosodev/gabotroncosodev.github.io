630ff693f449b8d440c2bb7cbe2bb68ff405cecb
Gabriel Troncoso <gabotroncosodev@gmail.com>
2022-10-24 20:33:20 -0300

    primera version publica lista, queda por optimizar el JS, medidas REM en sass y algunos font weight

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,34 @@
 const switchDarkLightMode = document.querySelector('#checkbox-dark-light');
 
 switchDarkLightMode.addEventListener('click', () =>{
-    // alert(switchDarkLightMode.checked);
+
+    /*!
+    ACLARACIÓN 
+    NO es el script más optimizado, pero es por tiempo, 
+    pronto haré un codigo más limpio
+    */
     if(switchDarkLightMode.checked){
-        // alert('si');
-        document.documentElement.style.setProperty('--bg-primary-color', '#000');
+        /**LIGHT */
+        document.documentElement.style.setProperty('--bg-primary-color', '#0095ff');
+        document.documentElement.style.setProperty('--bg-secondary-color', '#d6720f');
+        document.documentElement.style.setProperty('--bg-white-color', '#000000');
+        document.documentElement.style.setProperty('--bg-gray-color', '#7d7d7d');
+        document.documentElement.style.setProperty('--bg-black-color', '#ffffff');
+        document.documentElement.style.setProperty('--bg-black-one-color', '#e6e6e6');
+        document.documentElement.style.setProperty('--bg-black-two-color', '#ebebeb');
+        document.documentElement.style.setProperty('--bg-black-three-color', '#d2d2d2');
+        document.documentElement.style.setProperty('--bg-black-four-color', '#b0b0b0');
+    }else{        
+        /**Black */
+        document.documentElement.style.setProperty('--bg-primary-color', '#0095ff');
+        document.documentElement.style.setProperty('--bg-secondary-color', '#d6720f');
+        document.documentElement.style.setProperty('--bg-white-color', '#ffffff');
+        document.documentElement.style.setProperty('--bg-gray-color', '#9c9c9c');
+        document.documentElement.style.setProperty('--bg-black-color', '#000');
+        document.documentElement.style.setProperty('--bg-black-one-color', '#181818');
+        document.documentElement.style.setProperty('--bg-black-two-color', '#1D1D1D');
+        document.documentElement.style.setProperty('--bg-black-three-color', '#292929');
+        document.documentElement.style.setProperty('--bg-black-four-color', '#404040');
     }
 });
 
@@ -20,15 +44,15 @@ switchDarkLightMode.addEventListener('click', () =>{
 //     --bg-black-four-color:#404040;
 //   }
 
-const coloursDarkMode = {
-    secondarycolor : '#d6720f',
-    secondarycolor : '#d6720f',
-    primarycolor : '#0095ff',
-    whitecolor : '#ffffff',
-    graycolor : '#9c9c9c',
-    blackcolor : '#000',
-    blackonecolor : '#181818',
-    blacktwocolor : '#1D1D1D',
-    blackthreecolor : '#292929',
-    blackfoucolor : '#404040'
-}
+// const coloursDarkMode = {
+//     secondarycolor : '#d6720f',
+//     secondarycolor : '#d6720f',
+//     primarycolor : '#0095ff',
+//     whitecolor : '#ffffff',
+//     graycolor : '#9c9c9c',
+//     blackcolor : '#000',
+//     blackonecolor : '#181818',
+//     blacktwocolor : '#1D1D1D',
+//     blackthreecolor : '#292929',
+//     blackfoucolor : '#404040'
+// }
